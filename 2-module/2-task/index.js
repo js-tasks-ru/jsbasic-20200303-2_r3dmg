180e2f452c5b3c4cf655ce6f8fb55971dc14c784
Mihail <Mihail@MacBook-Pro.local>
2020-03-11 22:48:50 +0300

    2-module 1-3 tasks done

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -3,5 +3,18 @@
  * @param {Object} obj
  * @returns {Boolean}
  */
+
 function isEmpty(obj) {
+  for (let prop in obj) {
+    return false;
+  }
+  return true;
 }
+
+var schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "подъём";
+
+alert( isEmpty(schedule) ); // false
