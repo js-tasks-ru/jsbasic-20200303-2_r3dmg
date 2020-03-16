1abc7fd5fffec313005e90a31c2e8f811125a4ed
Mihail <Mihail@MacBook-Pro.local>
2020-03-16 12:05:37 +0300

    3 module 1-3 tasks done

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,5 +2,12 @@
  * @param {string} str
  * @returns {string}
  */
-function camelize(str) {
+function camelize(string) {
+  let arr = string.split('-');
+
+  let result = arr.map(function(item, index, array) {
+    return index === 0 ? item : item[0].toUpperCase() + item.slice(1,);
+  }).join('');
+
+  return result;
 }
