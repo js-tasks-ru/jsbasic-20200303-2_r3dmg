eaadf143d22985ce1622e35c43ba27625af2a920
Mihail <Mihail@MacBook-Pro.local>
2020-04-15 16:59:41 +0300

    7module 1 task done

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -4,6 +4,17 @@
  * @returns {Promise}
  */
 
+
+let button = document.createElement('button');
+
 function promiseClick(button) {
+   
+   return new Promise(resolve => {
+      button.addEventListener('click', () => {
+         resolve(event)
+      })
+   }, { once: true })
 
 }
+
+promiseClick(button).then((event) => console.log(event))
